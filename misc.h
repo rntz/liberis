9ccf5c1c3d6716287ee606588a08c99b11acba33
Michael Arntzenius <daekharel@gmail.com>
2012-02-26 23:50:10 -0500

    add UNREACHABLE, LIKELY, UNLIKELY to misc.h

--- misc.h
+++ misc.h
@@ -6,4 +6,30 @@
 #define XCAT(X,Y) X##Y
 #define CAT(X,Y) XCAT(X,Y)
 
+/* Clang defines __has_builtin, but other compilers don't. */
+#ifndef __has_builtin
+#define __has_builtin(x) 0
+#endif
+
+/* This macro is used to inform the compiler that a particular point is
+ * unreachable. Both clang and gcc use __builtin_unreachable for this purpose.
+ *
+ * I have no idea which gcc version introduced __builtin_unreachable, so I'm
+ * just gonna assume they all have it.
+ */
+#if __has_builtin(__builtin_unreachable) || defined __GNUC__
+#define UNREACHABLE (__builtin_unreachable())
+#else
+#define UNREACHABLE (assert(0 && "unreachable"))
+#endif
+
+/* TODO: document */
+#if __has_builtin(__builtin_expect) || defined __GNUC__
+#define LIKELY(x) (__builtin_expect(!!(x), 1))
+#define UNLIKELY(x) (__builtin_expect(!!(x), 0))
+#else
+#define LIKELY(x) (!!(x))
+#define UNLIKELY(x) (!!(x))
+#endif
+
 #endif
