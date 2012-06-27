c27551b473c8ff534461c40f26ccb2e21762edd1
Michael Arntzenius <daekharel@gmail.com>
2012-06-27 01:21:54 -0700

    refactor do_call, do_tailcall

--- vm.c
+++ vm.c
@@ -67,8 +67,14 @@ void do_builtin(vm_state_t *S, obj_t *obj, reg_t offset, nargs_t nargs)
     (void) nargs;            /* unused due to unimplemented variadic builtins */
 }
 
+/* Returns true iff we should next execute an OP_RETURN to simulate a
+ * tailcall. */
 static inline
-void do_call(vm_state_t *S, val_t funcv, reg_t offset, nargs_t nargs)
+bool do_call(vm_state_t *S,
+             bool tail_call,
+             val_t funcv,
+             reg_t offset,
+             nargs_t nargs)
 {
     obj_t *obj = VAL_OBJ(funcv);
     shape_t *tag = obj->tag;
@@ -81,64 +87,50 @@ void do_call(vm_state_t *S, val_t funcv, reg_t offset, nargs_t nargs)
         /* TODO: think very hard about what happens on control stack
          * overflow. */
 
-        /* Update our IP on control stack, so callee returns correctly. */
-        S->frame->ip = S->ip;
+        if (!tail_call) {
+            /* Update our IP on control stack, so callee returns correctly. */
+            S->frame->ip = S->ip;
 
-        /* Push callee's stack frame. Remember, control stack grows down. */
-        --S->frame;
-        S->frame->tag = FRAME_CALL;
+            /* Push callee's stack frame. Remember, control stack grows down. */
+            --S->frame;
+            S->frame->tag = FRAME_CALL;
+            /* No need to set frame's IP; callee will do that as necessary.
+             * frame's func gets set unconditionally, below. */
+
+            /* Shift our view of the register stack so our args are in the right
+             * place. */
+            S->regs += offset;
+        }
+        else {
+            /* No need to update frame's IP; callee will do that as necessary.
+             * No need to update frame's tag; already has appropriate value.
+             * frame's func gets set unconditionally, below.
+             */
+
+            /* Move down arguments into appropriate slots. */
+            memmove(S->regs, S->regs + offset, sizeof(val_t) * nargs);
+        }
+
+        /* Update frame. */
         S->frame->func = func;
-        /* No need to set frame's IP; callee will do that as necessary. */
 
         /* Jump into the function. */
         S->func = func;
         S->ip = S->func->proto->code;
-        S->regs += offset;
+
+        return false;            /* no OP_RETURN needed */
     }
     /* Calling builtins */
     else if (LIKELY(tag == SHAPE_TAG(builtin))) {
         do_builtin(S, obj, offset, nargs);
+        /* "Tail calling" a built-in is emulated using OP_RETURN. */
+        return tail_call;
     }
     /* Calling C closures */
     else if (LIKELY(tag == SHAPE_TAG(c_closure))) {
         assert(0 && "unimplemented"); /* TODO */
     }
     /* Calling everything else */
-    else {
-        eris_type_error("invalid function object");
-    }
-}
-
-/* Returns false iff we should next execute an OP_RETURN to simulate a
- * tailcall. */
-static inline
-bool do_tailcall(vm_state_t *S, val_t funcv, reg_t offset, nargs_t nargs)
-{
-    obj_t *obj = VAL_OBJ(funcv);
-    shape_t *tag = obj->tag;
-
-    if (LIKELY(tag == SHAPE_TAG(closure))) {
-        closure_t *func = OBJ_CONTENTS(closure, obj);
-        do_precall(S, func, offset, nargs);
-
-        /* Jump into the function, updating our control frame appropriately. */
-        S->func = S->frame->func = func; /* update frame's func */
-        S->ip = S->func->proto->code;
-        /* No need to update frame's tag; already has appropriate value.
-         * No need to update frame's IP; callee will do that as necessary. */
-
-        /* Move down the arguments into appropriate slots. */
-        memmove(S->regs, S->regs + offset, sizeof(val_t) * nargs);
-        return true;           /* no OP_RETURN needed */
-    }
-    else if (LIKELY(tag == SHAPE_TAG(builtin))) {
-        do_builtin(S, obj, offset, nargs);
-        return false;
-    }
-    else if (LIKELY(tag == SHAPE_TAG(c_closure))) {
-        assert(0 && "unimplemented"); /* TODO */
-        return false;
-    }
     else {
         eris_type_error("invalid function object");
     }
@@ -234,12 +226,13 @@ void eris_vm_run(vm_state_t *state)
          *  So if ARG2 = 3 and ARG3 = 4, the arguments are in registers 3-6
          *  inclusive.
          *
-         *  If OP is CALL or TAILCALL, ARG1 is an upval index. The upval does
-         *  not refer to a function to call; rather it refers to a ref cell
-         *  which refers to the function to call. Ideally, the upval should be
-         *  statically guaranteed to be a ref cell, so tag-checking it should be
-         *  unnecessary; for now, we do the check anyway. Checking that what the
-         *  ref-cell points to is a function is, however, necessary.
+         *  If OP is CALL_CELL or TAILCALL_CELL, ARG1 is an upval index. The
+         *  upval does not refer to a function to call; rather it refers to a
+         *  ref cell which refers to the function to call. Ideally, the upval
+         *  should be statically guaranteed to be a ref cell, so tag-checking it
+         *  should be unnecessary; for now, we do the check anyway. Checking
+         *  that what the ref-cell points to is a function is, however,
+         *  necessary.
          *
          *  If OP is CALL_REG or TAILCALL_REG, ARG1 is a register, which
          *  contains the function to be called. A type check is necessary as
@@ -251,28 +244,39 @@ void eris_vm_run(vm_state_t *state)
 #define CELL_FUNC CELL(ARG1)
 #define REG_FUNC REG(ARG1)
 
-      case OP_CALL_CELL:
-        do_call(&S, CELL_FUNC, ARG2, ARG3);
-        break;
+        {
+            val_t func;
+            bool tail_call;
 
-      case OP_CALL_REG:
-        do_call(&S, REG_FUNC, ARG2, ARG3);
-        break;
+          case OP_CALL_CELL:
+            func = CELL_FUNC;
+            tail_call = false;
 
-      case OP_TAILCALL_CELL:
-        /* We could avoid having do_tailcall return a bool by just requiring
-         * TAILCALL instrs to be followed by RETURN instrs; then do_tailcall can
-         * just increment IP if it wants to run the return instr. I'm not sure
-         * whether this would gain us anything, though.
-         */
-        if (do_tailcall(&S, CELL_FUNC, ARG2, ARG3))
+          call:
+            /* We could avoid having do_call return a bool by just requiring
+             * TAILCALL instrs to be followed by RETURN instrs; then do_call can
+             * just increment IP if it wants to run the return instr. I'm not
+             * sure whether this would gain us anything, though.
+             */
+            if (UNLIKELY(do_call(&S, tail_call, func, ARG2, ARG3)))
+                goto op_return;
             break;
-        goto op_return;
 
-      case OP_TAILCALL_REG:
-        if (do_tailcall(&S, REG_FUNC, ARG2, ARG3))
-            break;
-        goto op_return;
+          case OP_CALL_REG:
+            func = REG_FUNC;
+            tail_call = false;
+            goto call;
+
+          case OP_TAILCALL_CELL:
+            func = CELL_FUNC;
+            tail_call = true;
+            goto call;
+
+          case OP_TAILCALL_REG:
+            func = REG_FUNC;
+            tail_call = true;
+            goto call;
+        }
 
 
         /* Other instructions. */
@@ -290,29 +294,30 @@ void eris_vm_run(vm_state_t *state)
         break;
 
       op_return:
-      case OP_RETURN: (void) 0;
-        /* Put the return value where it ought to be. */
-        REG(0) = REG(ARG1);
+      case OP_RETURN: {
+          /* Put the return value where it ought to be. */
+          REG(0) = REG(ARG1);
 
-        ++S.frame;              /* pop control stack (it grows down) */
-        frame_tag_t frame_tag = *(frame_tag_t*) S.frame;
-        switch ((enum frame_tag) EXPECT_LONG(frame_tag, FRAME_CALL)) {
-          case FRAME_CALL: break;
-            /* C calls inevitably come through our API, so the API function that
-             * called us will do the necessary cleaning up. */
-          case FRAME_C_CALL: return;
-          default: IMPOSSIBLE("unrecognized or unimplemented frame tag: %u",
-                              frame_tag);
-        }
-        /* Okay, we're returning into an Eris closure. */
+          ++S.frame;              /* pop control stack (it grows down) */
+          frame_tag_t frame_tag = *(frame_tag_t*) S.frame;
+          switch ((enum frame_tag) EXPECT_LONG(frame_tag, FRAME_CALL)) {
+            case FRAME_CALL: break;
+              /* C calls inevitably come through our API, so the API function
+               * that called us will do the necessary cleaning up. */
+            case FRAME_C_CALL: return;
+            default: IMPOSSIBLE("unrecognized or unimplemented frame tag: %u",
+                                frame_tag);
+          }
+          /* Okay, we're returning into an Eris closure. */
 
-        /* We determine how far to pop the stack by looking at the argument
-         * offset given in the CALL instruction that set up our frame.
-         */
-        S.regs -= VM_ARG2(*S.frame->ip);
-        S.ip = S.frame->ip + 1; /* +1 to skip past the call instr. */
-        S.func = S.frame->func;
-        break;
+          /* We determine how far to pop the stack by looking at the argument
+           * offset given in the CALL instruction that set up our frame.
+           */
+          S.regs -= VM_ARG2(*S.frame->ip);
+          S.ip = S.frame->ip + 1; /* +1 to skip past the call instr. */
+          S.func = S.frame->func;
+          break;
+      }
 
       case OP_IF:
         do_cond(&S, !VAL_IS_NIL(REG(ARG1)));
@@ -324,36 +329,37 @@ void eris_vm_run(vm_state_t *state)
 
         /* NB. logical CLOSE instr spans multiple (>= 2) instr_ts.
          * TODO: document format of CLOSE instr. */
-      case OP_CLOSE: (void) 0;
-        upval_t nupvals_upvals = ARG2; /* # upvals from parent upvals */
-        upval_t nupvals_regs = ARG3;   /* # upvals from registers */
-        upval_t nupvals = nupvals_upvals + nupvals_regs;
-        obj_t *funcobj = NEW_WITH(closure, upvals, nupvals);
-        DEST = OBJ_VAL(funcobj);
-        closure_t *func = OBJ_CONTENTS(closure, funcobj);
+      case OP_CLOSE: {
+          upval_t nupvals_upvals = ARG2; /* # upvals from parent upvals */
+          upval_t nupvals_regs = ARG3;   /* # upvals from registers */
+          upval_t nupvals = nupvals_upvals + nupvals_regs;
+          obj_t *funcobj = NEW_WITH(closure, upvals, nupvals);
+          DEST = OBJ_VAL(funcobj);
+          closure_t *func = OBJ_CONTENTS(closure, funcobj);
 
-        /* TODO: carefully consider manually optimizing this. */
+          /* TODO: carefully consider manually optimizing this. */
 
-        /* Actually construct the closure. */
-        ++S.ip;
-        arg_t which_func = VM_ARG0(*S.ip);
-        func->proto = S.func->proto->local_funcs[which_func];
+          /* Actually construct the closure. */
+          ++S.ip;
+          arg_t which_func = VM_ARG0(*S.ip);
+          func->proto = S.func->proto->local_funcs[which_func];
 
-        /* We read the indices from which to populate the new closure's upvals
-         * from the instructions following the OP_CLOSE. */
-        size_t i = 1;
-        for (; i <= nupvals_upvals; ++i) {
-            upval_t idx = VM_ARGN(*(S.ip + i / sizeof(instr_t)),
-                                  i % sizeof(instr_t));
-            func->upvals[i] = S.func->upvals[idx];
-        }
-        for (; i <= nupvals; ++i) {
-            upval_t idx = VM_ARGN(*(S.ip + i / sizeof(instr_t)),
-                                  i % sizeof(instr_t));
-            func->upvals[i] = REG(idx);
-        }
-        S.ip += INTDIV_CEIL(1 + nupvals, sizeof(instr_t));
-        break;
+          /* We read the indices from which to populate the new closure's upvals
+           * from the instructions following the OP_CLOSE. */
+          size_t i = 1;
+          for (; i <= nupvals_upvals; ++i) {
+              upval_t idx = VM_ARGN(*(S.ip + i / sizeof(instr_t)),
+                                    i % sizeof(instr_t));
+              func->upvals[i] = S.func->upvals[idx];
+          }
+          for (; i <= nupvals; ++i) {
+              upval_t idx = VM_ARGN(*(S.ip + i / sizeof(instr_t)),
+                                    i % sizeof(instr_t));
+              func->upvals[i] = REG(idx);
+          }
+          S.ip += INTDIV_CEIL(1 + nupvals, sizeof(instr_t));
+          break;
+      }
 
       default:
         IMPOSSIBLE("unrecognized or unimplemented opcode: %u", OP);
